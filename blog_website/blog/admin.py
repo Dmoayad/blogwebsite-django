7dc3ddb1750bbb13a6cfb68a9068b64ca11cd176
Moayad <dmoayad@hotmail.com>
2025-04-12 19:55:27 +0300

    fixed a few bugs, and displaying tables in admin properly

--- blog_website/blog/admin.py
+++ blog_website/blog/admin.py
@@ -4,8 +4,17 @@ from .models import User, Post, Comment, Category
 class UserAdmin(admin.ModelAdmin):
     list_display = ("id","username", "email", "password")
 
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "content", "category", "date_published")
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("id", "post_id", "user_id", "content", "date_posted")
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
+
 admin.site.register(User, UserAdmin)
-admin.site.register(Post)
-admin.site.register(Comment)
-admin.site.register(Category)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
+admin.site.register(Category, CategoryAdmin)
 
--- blog_website/blog/models.py
+++ blog_website/blog/models.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-
+from django.utils import timezone
 from django.db import models
 from django.db.models import SET_NULL
 from django.contrib.auth.hashers import make_password, check_password
@@ -30,15 +30,17 @@ class User(models.Model):
         return check_password(raw_password, self.password)
 
     def __str__(self):
-        return f"{self.id} {self.username}"
+        return f"{self.id}"
 
 class Post(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=255)
     content = models.TextField(max_length=255)
     category = models.ForeignKey('Category', default=None, null=True, on_delete=SET_NULL)
-    date_published = models.DateTimeField(default=datetime.now(), null=True, blank=True)
+    date_published = models.DateTimeField(default=timezone.now, null=True, blank=True)
 
+    def __str__(self):
+        return f"{self.id}"
 
 class Comment(models.Model):
     id = models.AutoField(primary_key=True)
@@ -54,5 +56,5 @@ class Category(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
 
-    # def __str__(self):
-    #     return f"{self.name}"
+    def __str__(self):
+        return f"{self.name}"
